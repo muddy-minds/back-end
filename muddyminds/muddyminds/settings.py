fccbb9b56aee8c9b99ff0c0f49878c31d545e6cb
Sem8 <mssemmi8@gmail.com>
2019-07-29 22:43:13 -0400

    migrations in sqlite3 finally works

--- muddyminds/muddyminds/settings.py
+++ muddyminds/muddyminds/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
     'rest_framework',
+    'adv'
     
-             
+                
 
 ]
 
