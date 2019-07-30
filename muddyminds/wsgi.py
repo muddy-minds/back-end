d125b6a59d52da8427f3ebabdffe4cbe307acda1
Matt Basile <matthewbasile2@gmail.com>
2019-07-30 16:24:20 -0400

    establishing main procfile

--- muddyminds/wsgi.py
+++ muddyminds/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muddyminds.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'build_week.settings')
 
 application = get_wsgi_application()
