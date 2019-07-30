ff9c2030db8bb191f09a8f4c30fb0e188deb630f
guillsav <guillsav@gmail.com>
2019-07-30 12:40:10 -0400

    migration

--- muddyminds/adv/models.py
+++ muddyminds/adv/models.py
@@ -2,6 +2,8 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+
+
 class Room(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
@@ -10,12 +12,13 @@ class Room(models.Model):
     east = models.IntegerField(default=0)
     west = models.IntegerField(default=0)
 
+
 class Player(models.Model):
     name = models.CharField(max_length=50)
     items = ArrayField(
-            models.CharField(max_length=50, blank=True),
-            size=100,
-        )
+        models.CharField(max_length=50, blank=True),
+        size=100,
+    )
     description = models.CharField(max_length=500)
     health_points = models.FloatField()
     lives = models.IntegerField(default=3)
