1d8cf6c8a3ffdd660f917cc0cb50ab0af39fc9c2
Francis Chen <fncis.chen@gmail.com>
2019-07-30 08:43:53 -0700

    added cors middleware

--- muddyminds/admin/apps.py
+++ muddyminds/admin/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
+class CorsMiddleware(object):
+    def process_response(self, req, resp):
+        response["Access-Control-Allow-Origin"] = "*"
+        return response
 
 class AdminConfig(AppConfig):
     name = 'admin'
