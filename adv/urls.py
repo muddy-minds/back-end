e506d23118387cd9e838bbcbf7c1a106afe4e029
guillsav <guillsav@gmail.com>
2019-07-31 11:24:10 -0400

    removed extra files

--- adv/urls.py
+++ adv/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-# from .views import RoomViewSet, PlayerViewSet
-
 
 router = DefaultRouter()
-# router.register('rooms', RoomViewSet)
-# router.register('players', PlayerViewSet)
+
 
 app_name = 'adv'
 
