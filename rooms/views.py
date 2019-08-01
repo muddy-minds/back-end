122029c1172ed939d6eae39806d94aebe8305a65
Sem8 <mssemmi8@gmail.com>
2019-08-01 11:45:13 -0400

    changed player and room views to ModelViewSet so a single player and room object can be retrieved by id

--- rooms/views.py
+++ rooms/views.py
@@ -3,9 +3,7 @@ from rest_framework import viewsets, mixins
 from adv.models import Room
 from .serializers import RoomSerializer
 
-class RoomViewSet(viewsets.GenericViewSet, 
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
+class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
