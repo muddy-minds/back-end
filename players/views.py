0b22f30724d98ea155701253060390a31117f973
guillsav <guillsav@gmail.com>
2019-08-01 17:17:46 -0400

    modified adding player route

--- players/views.py
+++ players/views.py
@@ -8,17 +8,17 @@ class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
-    def get_queryset(self):
-        """Retrive the player for the authenticated user"""
-        return self.queryset.filter(name=self.request.name)
+    # def get_queryset(self):
+    #     """Retrive the player for the authenticated user"""
+    #     return self.queryset.filter(name=self.request.name)
 
     
-    def get_serializer_class(self):
-        """Return the serializer class"""
-        if self.action == 'retrieve':
-            return serializers.PlayerSerializer
-        
-        return self.serializer_class
+    # def get_serializer_class(self):
+    #     """Return the serializer class"""
+    #     if self.action == 'retrieve':
+    #         return serializers.PlayerSerializer
+
+    #     return self.serializer_class
 
     def create_player(sef, serializer):
         """Create a new player"""
