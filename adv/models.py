6aad295f5a4262865c299ce3681793b57685e05c
Francis Chen <fncis.chen@gmail.com>
2019-08-01 10:51:15 -0700

    updated migrations and items

--- adv/models.py
+++ adv/models.py
@@ -11,16 +11,14 @@ from datetime import datetime
 #         kwargs['max_length'] = 104
 #         super().__init__(*args, **kwargs)
 
-
 class Room(models.Model):
-    name = models.CharField(max_length=50, default="DEFAULT TITLE")
-    description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION" )
-    items = JSONField()
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    items = JSONField(default={})
     north = models.IntegerField(default=0)
     south = models.IntegerField(default=0)
     east = models.IntegerField(default=0)
     west = models.IntegerField(default=0)
-
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
@@ -29,63 +27,46 @@ class Room(models.Model):
             print("That room does not exist")
         else:
             if direction == "n":
-                self.n_to = destinationRoomID
+                self.north = destinationRoomID
             elif direction == "s":
-                self.s_to = destinationRoomID
+                self.south = destinationRoomID
             elif direction == "e":
-                self.e_to = destinationRoomID
+                self.east = destinationRoomID
             elif direction == "w":
-                self.w_to = destinationRoomID
+                self.west = destinationRoomID
             else:
                 print("Invalid direction")
                 return
             self.save()
-
     def __str__(self):
         return self.name
-
 # https://django-postgres-extensions.readthedocs.io/en/latest/arrays.html
 # array field methods 
 
 class Player(models.Model):
     name = models.CharField(max_length=50)
-    items = JSONField()
+    items = JSONField(default={})
     description = models.CharField(max_length=500)
     health_points = models.FloatField()
     lives = models.IntegerField(default=3)
     room_id = models.ForeignKey(Room, models.SET_NULL, blank=True, null=True)
 
-    def getItem(self, item):
-        if isInstance(RoomItems) & item.room_id == room_id:
-            item.player_id = id 
-            Player.objects.update(items = ArrayAppend('items', 
-            {"name": item.name, 
-            "id": item.id,
-            "description": item.description, 
-            "room_id": item.room_id,
-            "player_id": item.player_id,
-            "item_type": item.__class__.__name__,
-            "health_points": item.health_points,
-            "damage_points": item.damage_points,
-            "some_points": item.some_points}
-            ))
+    # def getItem(self, item):
+    #     if isInstance(RoomItems) & item.room_id == room_id:
+    #         item.player_id = id 
+    #         Player.objects.update(items = ArrayAppend('items', id))
 
 
-    def dropItem(self, item):
-        if item.player_id == id:
-            itemInArrayField = Player.objects.filter(items___name = item.name)
-            if itemInArrayField != None:
-                item.player_id = None
-                Player.objects.update(items = ArrayRemove('items', itemInArrayField
-                ))
+    # def dropItem(self, item):
+    #     if item.player_id == id:
+    #         itemInArrayField = Player.objects.filter(items___name = item.name)
+    #         if itemInArrayField != None:
+    #             item.player_id = None
+    #             Player.objects.update(items = ArrayRemove('items', id))
+                
+    # def readItem(self, item):
+    #     return item.description 
 
-    def readItem(self, item):
-        return item.description 
-
-    def eatItem(self, foodItem):
-        if isInstance(foodItem, FoodItem):
-            health_points += foodItem.health_points
-    
     def enterNewRoom(self, command):
         if type(command) == str:
             if command == "n":
@@ -108,11 +89,6 @@ class Player(models.Model):
                     room_id = Room.objects.get(id=room_id).east 
                 else:
                     print("We cannot go east")
-
-    def attackNPC(self, NPC, item):
-        # check if NPC is Npc and a weapon exists in ArrayField
-        if isinstance(NPC, Npc) & Player.objects.filter(items__contains=[item] & isinstance(item, Weapons)):
-            NPC.health_points -= item.damage_points
 
     def befriendNPC(self, NPC):
         if isinstance(NPC, Npc) & NPC.friend_id != None:
@@ -137,23 +113,23 @@ class Npc(models.Model):
 
 
 
-class RoomItems(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    room_id = models.ForeignKey(Room, models.SET_NULL, blank=True, null=True)
-    player_id = models.ForeignKey(
-        Player, models.SET_NULL, blank=True, null=True)
+# class RoomItems(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.CharField(max_length=500)
+#     room_id = models.ForeignKey(Room, models.SET_NULL, blank=True, null=True)
+#     player_id = models.ForeignKey(
+#         Player, models.SET_NULL, blank=True, null=True)
 
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
-class FoodItem(RoomItems):
-    health_points = models.FloatField()
-
-
-class Toys(RoomItems):
-    some_points = models.FloatField()
+# class FoodItem(RoomItems):
+#     health_points = models.FloatField()
 
 
-class Weapons(RoomItems):
-    damage_points = models.FloatField()
+# class Toys(RoomItems):
+#     some_points = models.FloatField()
+
+
+# class Weapons(RoomItems):
+#     damage_points = models.FloatField()
